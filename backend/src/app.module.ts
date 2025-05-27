01e08b8b56ffebcda1856136aa3ff0521a985443
herediaalejo <heredia.alejo.manuel@gmail.com>
2025-05-27 17:00:16 -0300

    Fuentes y db normalizada

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,18 +7,20 @@ import { User } from './users/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    password: '2314',
-    port: 3306,
-    username: 'root',
-    database: 'crud',
-    entities: [User],
-    synchronize: true,
-  }),
-  UsersModule,
-  AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      password: '',
+      port: 3306,
+      username: 'root',
+      database: 'crud',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
